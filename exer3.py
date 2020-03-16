d81e3a2df4f5489b279ecbfc85f3c99e6d1104ad
Larissa Alves <larissa.laritt@icloud.com>
2020-03-15 23:04:19 -0300

    Informa quantos anos a pessoa terá no final de 2020

---
+++ exer3.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um número: '))
+
+num2 = int(input('Digite outro número: '))
+
+print(
+f' Operações com esses números\nSoma: {num + num2}\nMultiplicação: {num  *num2}\nDivisão Inteira: {num // num2}\nResto: {num % num2}'
+)
