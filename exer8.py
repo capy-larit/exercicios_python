4f4e14096d247171aa6b42f05516891264ac76ef
Larissa Alves <larissa.laritt@icloud.com>
2020-03-15 23:11:25 -0300

    Análise combinatória

---
+++ exer8.py
@@ -0,0 +1,7 @@
+print('=' * 50)
+x = int(input('Digite o número de camisetas: '))
+y = int(input('Digite o número de calças: '))
+z = int(input('Digite o número de pares de sapatos: '))
+
+print('=' * 50)
+print(f'{x * y * z} maneiras diferentes de se vestir')
