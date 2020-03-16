1c8e117086558d4e9f4b8301fb29b2605f4da59e
Larissa Alves <larissa.laritt@icloud.com>
2020-03-15 23:09:34 -0300

    Calcula a área e o perímetro de um círculo

---
+++ exer6.py
@@ -0,0 +1,10 @@
+raio = int(input('Digite o valor do raio: '))
+
+pi = 3.141592
+
+# A = Pi * r * r
+# P = 2 * pi * r
+
+print(
+  f'Área do círculo: {pi * raio * raio}\nPerímetro do círculo: {2 * pi * raio}'
+)
