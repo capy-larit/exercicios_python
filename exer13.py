470a7edf34e57ab33a47af4ca8e4e1c335cc5b22
Larissa Alves <larissa.laritt@icloud.com>
2020-03-15 23:19:32 -0300

    Calcula a média do aluno

---
+++ exer13.py
@@ -0,0 +1,5 @@
+nac = float(input('Digite sua nota da NAC: '))
+am = float(input('Digite sua nota da AM: '))
+ps = float(input('Digite sua nota da PS: '))
+
+print(f'Sua média é: {(2*nac+3*am+5*ps)/10}')
