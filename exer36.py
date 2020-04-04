0e0517d1fa78facc9778ee7a60e3831c10db816b
Larissa Alves <larissa.laritt@icloud.com>
2020-04-04 20:54:04 -0300

    Números iguais ou diferentes.

---
+++ exer36.py
@@ -0,0 +1,12 @@
+'''
+Faça um algoritmo em Python para ler dois números inteiros e informar se estes números são iguais ou diferentes. 
+'''
+
+numero = int(input('Digite um número: '))
+numero_1 = int(input('Digite outro número: '))
+
+if numero == numero_1:
+  print('Os números digitados são iguais.')
+
+else:
+  print('Os números digitados são diferentes.')
