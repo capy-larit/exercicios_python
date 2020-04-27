2c01d7309f819e4ed9dfdc7de32a95ddafbe93b2
capy-larit <larissa.laritt@icloud.com>
2020-04-27 17:34:34 -0300

    Tabuada do 5 com while.

---
+++ exer45.py
@@ -0,0 +1,11 @@
+'''
+Faça um programa em Python que mostre na tela a tabuada do 5.
+'''
+
+multiplicador = 0
+tabuada = 5
+while multiplicador <=10:
+  print(
+    '5 x {} = {}'.format(multiplicador, 5 * multiplicador)
+  )
+  multiplicador += 1 
