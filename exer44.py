972241901c2dc3cc888b2d96ae46ee73db925254
capy-larit <larissa.laritt@icloud.com>
2020-04-27 17:32:40 -0300

    Números pares com while.

---
+++ exer44.py
@@ -0,0 +1,18 @@
+'''
+Elabore um programa em Python que mostre na tela somente os números
+pares entre 100 e 200. Incluindo-os.
+'''
+
+# Exemplo 1
+while numero in range(98,199) and numero <= 200:
+  if numero % 2 == 0:
+   numero = numero + 2
+   print(numero)
+
+
+# Exemplo 2
+num=100
+
+ while num <= 200:
+   print(num)
+   num += 2
