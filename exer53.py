175646b8b6039e5e7b8b1674d1c42b7c063a1369
capy-larit <larissa.laritt@icloud.com>
2020-09-05 18:16:27 -0300

    exer53.py - Adiciona exercício ultilizando while.
    - Sequência de números inteiros: 21 20 19 18 17 16 15.

---
+++ exer53.py
@@ -0,0 +1,10 @@
+"""
+Faça um programa em Python que mostre na tela a seguinte sequencia de
+números inteiros: 21 20 19 18 17 16 15.
+"""
+
+cont = 21
+
+while cont >= 15:
+    print(cont)
+    cont -= 1
