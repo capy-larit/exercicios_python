7a01ebbec9d6467a67f7dac482ff22027ecf253f
capy-larit <larissa.laritt@icloud.com>
2020-09-05 17:58:08 -0300

    exer50.py - Adiciona exercício utilizando while. - Sequência de números inteiros.

---
+++ exer50.py
@@ -0,0 +1,10 @@
+"""
+Faça um programa em Python que mostre na tela a seguinte sequencia de
+números inteiros: 1 2 3 4 5 6 7 8 9 10.
+"""
+
+cont = 1
+
+while cont <= 10:
+    print(cont)
+    cont += 1
