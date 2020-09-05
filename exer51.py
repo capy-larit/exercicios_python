d2a2a6e5a4dbe75198ceb5f53c1c52b409d4faf2
capy-larit <larissa.laritt@icloud.com>
2020-09-05 18:08:37 -0300

    exer51.py - Adiciona exercício ultilizando while.
    - Sequência de números inteiros pulando de dois em dois.

---
+++ exer51.py
@@ -0,0 +1,10 @@
+"""
+Faça um programa em Python que mostre na tela a seguinte sequencia de
+números inteiros: 1 3 5 7 9 11 13 15 17.
+"""
+
+cont = 1
+
+while cont <= 17:
+    print(cont)
+    cont += 2
