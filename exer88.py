23697035e5254f251fce8dadd68f75aa3627efe5
capy-larit <larissa.laritt@icloud.com>
2020-09-07 20:10:56 -0300

    exer88.py - Adiciona exercício ultilizando dicionário.
    - O programa possui um dicionário com o nome de 5
      pessoas como chave e colégio que estudam como valor.

---
+++ exer88.py
@@ -0,0 +1,7 @@
+"""
+Crie um dicionário com o nome de 5 pessoas, sendo o nome como chave e o
+colégio que estuda como valor.
+"""
+
+dic = {'Larissa' : 'Fiap', 'Pamela' : 'Etec 1', 'Lucas' : 'Deliberato', 'Lais' : 'Etec 2', 'Giselly' : 'Deliberato 2' }
+print(dic)
